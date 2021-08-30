e542e7ef974d3af3508dc785a382769a62d2315a
Mithril-NTU <d08944012@ntu.edu.tw>
2021-08-30 16:39:36 +0800

    -

--- grid_dpr_lrsq.sh
+++ grid_dpr_lrsq.sh
@@ -2,11 +2,16 @@
 
 # basic seting
 config=$1
+gpu=$2
 
 task(){
 # Set up train command
-train_cmd="python3 main.py"
+train_cmd="CUDA_VISIBLE_DEVICES=$gpu python3 main.py"
 train_cmd="${train_cmd} --config ${config}"
+train_cmd="${train_cmd} --fix_q_encoder --fix_ctx_encoder"
+train_cmd="${train_cmd} --result_dir ./runs/extra_nn"
+train_cmd="${train_cmd} --tfboard_log_dir ./tfboard_logs/extra_nn"
+#train_cmd="${train_cmd} --isWithoutWeight"
 wd=0.0
 
 # Print out all parameter pair
@@ -19,12 +24,13 @@ do
         do
             for omega in 1.0 0.0625 0.00390625 0.000244140625
             do
-                for r in -28 -30.0 -32.0
+                for r in -4 -8 -16 -32 -1 -2
                 do
                     cmd="${train_cmd} --learning_rate ${lr}"
                     #cmd="${cmd} --bratio ${br}"
                     cmd="${cmd} --bsize_i ${br} --bsize_j ${br}"
                     cmd="${cmd} --weight_decay ${wd}"
+                    cmd="${cmd} --projection_dim 128"
                     cmd="${cmd} --omega ${omega}"
                     cmd="${cmd} --imp_r ${r}"
                     cmd="${cmd} --seed ${seed}"
@@ -41,4 +47,4 @@ task
 wait
 
 # Run
-task | xargs -0 -d '\n' -P 1 -I {} sh -c {}
+#task | xargs -0 -d '\n' -P 1 -I {} sh -c {}
